2ae07e52fd48152847e1c34498f1a06b8bc8836c
Shubham Sharma <shubham.sharma5@BLRETV-P1EYG8WN.local>
2022-06-07 08:42:57 +0530

    basic

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author shubham.sharma5
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first app.");
+
+	}
+
+}
